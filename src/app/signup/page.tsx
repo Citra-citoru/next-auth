832980a46aa0336b36675e1af8fc825266ffe3e7
citra-citoru <citra.rahman@happy5.co>
2023-08-16 23:07:50 +0300

    wip

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import { Axios } from "axios";
+export default function Signup() {
+    return (
+      <div className="flex-auto">
+          <h1 className="text-center text-2XL">Signup</h1>
+      </div>
+    )
+  }
+  
