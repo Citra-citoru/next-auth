e24d7dec4f8ab8d63326b36a6083b448e9a9c027
citra-citoru <citra.rahman@happy5.co>
2023-08-20 17:20:20 +0300

    add user model

---
+++ src/model/user.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+}, { timestamps: true }
+);
+
+module.exports = mongoose.model('user', userSchema);
